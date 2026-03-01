656bc9253721ee2d6cbaece49a1c53f3d50c1108
Jarek Polowy <jarekpolowy@gmail.com>
2026-03-01 20:06:22 +0000

    temporarily disable obligatory login (#23)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,10 @@ import LoginButton from '@/components/custom/LoginButton';
 const LoginPage = () => {
   return (
     <>
-      <LoginButton />
+      <div className='flex flex-col items-center gap-6 p-4 sm:p-6 min-h-screen'>
+        <div className='text-center mb-4'>Please log in to continue</div>
+        <LoginButton />
+      </div>
     </>
   );
 };

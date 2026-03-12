d0ba5175f5b7eaa0ccf12866403ba900dc79b156
Jarek Polowy <jarekpolowy@gmail.com>
2026-03-12 20:16:05 +0000

    store messages in db (#8)

--- src/services/unentropiaServices.tsx
+++ src/services/unentropiaServices.tsx
@@ -1,8 +1,12 @@
 import {unentropiaApiInstance} from './instances';
 
-export const postChatMessage = async (sessionId: string, message: string) => {
+export const postChatMessage = async (sessionId: string, message: string, userId: string | number | null) => {
   try {
-    const {data, status} = await unentropiaApiInstance.post('/api/v1/chat/', {session_id: sessionId, message: message});
+    const {data, status} = await unentropiaApiInstance.post('/api/v1/chat/', {
+      session_id: sessionId,
+      message: message,
+      user_id: userId,
+    });
     return {data, status};
   } catch (error) {
     console.error('Error posting chat message:', error);
